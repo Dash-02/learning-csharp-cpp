e2106984cbd1793f2c61d20677f93afb79ebf5b5
Dash-02 <dasha.semivolos@gmail.com>
2024-06-25 20:48:39 +0300

    ticket19_sharp

---
+++ csharp/bilet19_sharp/bilet19_sharp/Program.cs
@@ -0,0 +1,49 @@
+﻿// Мальчик может бегать в три раза быстрее, чем ходить. Скорость его 
+// ходьбы равна 4 км/ч. Он принял участие в марафонском забеге, но сошел с 
+// дистанции, пробежав только Х км. Сколько времени он потратил на 
+// преодоление этого расстояния.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bilet19_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int walkBoy = 4;
+            double runBoy = 3 * walkBoy; // скорость мальчика при беге
+            double hour = 1;
+
+            Console.Write("Какую дистанцию пробежал мальчик? Введите в км: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+
+            if (length > 0)
+            {
+                hour = length  / runBoy;
+            }
+            else
+            {
+                Console.Write("Дистанция не может быть меньше 0 км! Повторите: ");
+                length = Convert.ToDouble(Console.ReadLine());
+            }
+
+            hour = Math.Round(hour, 2);
+
+            string text = $"На преодоление {length} км мальчик потратил {hour} часов";
+
+            Console.WriteLine(text);
+
+            string path = "bilet19_sharp.txt";
+            using (StreamWriter writer = new StreamWriter(path, false)) { 
+                writer.WriteLine(text);
+                Console.WriteLine("Файл записан");
+            }
+        }
+    }
+}

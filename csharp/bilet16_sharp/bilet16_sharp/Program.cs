08e1463dd74afb8396bc17ee374d7383da019428
Dash-02 <dasha.semivolos@gmail.com>
2024-06-28 14:31:39 +0300

    ticket16_sharp

---
+++ csharp/bilet16_sharp/bilet16_sharp/Program.cs
@@ -0,0 +1,43 @@
+﻿// Автомобиль на каждом из пяти одинаковых по длине участках дороги 
+// шел с известной средней скоростью. Составьте алгоритм и программу для 
+// определения средней скорости на всем пути.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bilet16_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sumSpeed = 0;
+            double averageSpeed;
+            double[] speeds = new double[5];
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.Write($"Введите скорость автомобиля на {i+1} участке дороги (км/ч): ");
+                speeds[i] = Convert.ToDouble(Console.ReadLine());
+
+                sumSpeed += speeds[i];
+            }
+
+            averageSpeed = sumSpeed / speeds.Length;
+
+            string text = $"Средняя скорость автомобиля на всем пути составляет {averageSpeed} км/ч";
+            string path = "bilet16_sharp.txt";
+
+            Console.WriteLine(text);
+
+            using (StreamWriter writer = new StreamWriter(path, false)) 
+            {
+                writer.WriteLine(text);
+                Console.WriteLine("Файл записан");
+            }
+        }
+    }
+}

88f9ded72c9bdbade9f92e8da5bdc99185f45386
Dash-02 <dasha.semivolos@gmail.com>
2024-06-30 23:52:26 +0300

    ticket13_cpp_needfix

---
+++ cplusplus/bilet13_cpp/bilet13_cpp/bilet13_cpp.cpp
@@ -0,0 +1,37 @@
+﻿// Введите по 4 оценки студента. Вычислить количество неуспевающих 
+// студентов и средний балл группы по всем предметам. Количество студентов
+// заранее неизвестно.
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+    const int MAX_STUDENTS = 100;
+    const int NUM_GRADES = 4;
+    int count_student;
+    int badMarkStudent = 0;
+    double averMark = 0;
+
+    cout << "Введите количество студентов: ";
+    cin >> count_student;
+
+    int studentsMark[MAX_STUDENTS][NUM_GRADES];
+
+    for (int i = 0; i < count_student; i++) {
+        cout << "Введите оценки для ученика " << i + 1 << ":" << endl;
+        for (int j = 0; j < NUM_GRADES; j++) {
+            cout << "Оценка " << j + 1 << ": ";
+            cin >> studentsMark[i][j];
+            if (studentsMark[i][j] == 1 || studentsMark[i][j] == 2)
+            {
+                badMarkStudent++;
+            }
+            averMark += studentsMark[i][j];
+        }
+    }
+    averMark = averMark / static_cast<double>(count_student * NUM_GRADES);
+    cout << "Средний балл группы: " << averMark << "\nКоличество неуспевающих студентов: " << badMarkStudent;
+} 

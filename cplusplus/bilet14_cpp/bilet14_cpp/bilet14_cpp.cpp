eff31af5ee0a69035371400993e3ffd146dea16b
Dash-02 <dasha.semivolos@gmail.com>
2024-06-30 19:41:51 +0300

    ticket14_cpp

---
+++ cplusplus/bilet14_cpp/bilet14_cpp/bilet14_cpp.cpp
@@ -0,0 +1,73 @@
+﻿// Исследуется структура автомобильного потока на дороге путем 
+// последовательной регистрации проезжающих автомобилей по 4 признакам,
+// соответствующим типам автомобилей. В конце регистрации определяется
+// процентное соотношение этих типов к общему числу автомобилей.Кодируйте
+// тип автомобиля по признакам : легковые - 1, грузовые до 5т. - 2, грузовые
+// свыше 5т. - 3, транспорт - 4. Признак окончания ввода любое отрицательное число
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+
+    int type = 0;
+    int countAuto = 0; 
+    int count1 = 0;
+    int count2 = 0;
+    int count3 = 0;
+    int count4 = 0;
+    int percent1 = 0;
+    int percent2 = 0;
+    int percent3 = 0;
+    int percent4 = 0;
+    
+    while (type >= 0)
+    {
+        cout << "Укажите тип автомобиля:\n 1 - легковые\n 2 - грузовые до 5т.\n 3 - грузовые свыше 5т.\n 4 - транспорт\n-1 - выход\n";
+        cin >> type;
+
+        if (type == 1)
+        {
+            count1++;
+        } 
+        else if (type == 2)
+        {
+            count2++;
+        }
+        else if (type == 3)
+        {
+            count3++;
+        }
+        else if (type == 4)
+        {
+            count4++;
+        }
+        else if (type < 0)
+        {
+            countAuto = count1 + count2 + count3 + count4;
+            percent1 = (count1 * 100) / countAuto;
+            percent2 = (count2 * 100) / countAuto;
+            percent3 = (count3 * 100) / countAuto;
+            percent4 = (count4 * 100) / countAuto;
+
+            cout << "Из " << countAuto << " автомобилей: " << percent1 << "% - легковые, " << percent2 << "% - грузовые до 5т, " << percent3 << "% - грузовые свыше 5т, " << percent4 << "% - транспорт\n";
+        }
+        else
+        {
+            cout << "Пожалуйста, повторите. ";
+        }
+    }
+    type = 0;
+
+    ofstream out;
+    out.open("bilet14_cpp.txt");
+    if (out.is_open())
+    {
+        out << "Из " << countAuto << " автомобилей: " << percent1 << "% - легковые, " << percent2 << "% - грузовые до 5т, " << percent3 << "% - грузовые свыше 5т, " << percent4 << "% - транспорт\n";
+        cout << "Файл записан";
+    }
+    out.close();
+ }
---
+++ csharp/bilet14_sharp/bilet14_sharp/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bilet14_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cash = 100;
+            int costBull = 10;
+            int costCow = 5;
+            double costCalf = 0.5;
+            int countAll = 100;
+            int count = 0;
+            int countBull = 5;
+            int countCow = 10;
+            int countCalf = 50;
+            double sumCount = 0;
+            double sumCost = 0;
+
+            int[] maxCounts = {10, 20, 200};
+
+            for (int i = 0; i < maxCounts.Length; i++) 
+            {
+                sumCost += maxCounts[i];
+                Console.WriteLine($"sumCost - {sumCost}");
+                for (int j = 0; j < maxCounts.Length; j++)
+                {
+                    while (sumCost != 100) {
+                        maxCounts[j]--;
+                        Console.WriteLine(maxCounts[j]);
+                        if (j == 0) {
+                            maxCounts[j] *= costBull;
+                        } else if (j == 1)
+                        {
+                            maxCounts[j] *= costCow;
+                        }
+                        else if (j == 2)
+                        {
+                            maxCounts[j] *= (5 / 10);
+                        }
+                        break;
+                    }
+
+                }
+            }
+            
+            //countBull = Convert.ToInt32(cash / costBull);
+            //countCow = Convert.ToInt32(cash / costCow);
+            //countCalf = Convert.ToInt32(cash / costCalf);
+
+            //Console.WriteLine($"{}");
+            //sumCount = costBull * countBull + costCow * countCow + costCalf * countCalf;
+            //while (sumCount != 100) 
+            //{
+            //    // sumCount = countAll / countBull + countAll / countCow + countAll / countCalf;
+            //    sumCount = costBull * countBull + costCow * countCow + costCalf * countCalf;
+            //    // sumCost = 
+            //    Console.WriteLine($"{sumCount} sumCount");
+            //    if (sumCount == countAll) 
+            //    {
+            //        Console.WriteLine("g");
+            //        break;
+            //    }
+            //    count++;
+            //    countBull--;
+            //    countCow--;
+            //    countCalf--;
+            //    Console.WriteLine($"count - {count}, countBull - {countBull}, countCow - {countCow}, countCalf - {countCalf}");
+            //}
+            //Console.WriteLine("sumCount = " + sumCount);
+            string text = $"На {cash} руб. можно купить {countBull} быков, {countCow} коров, {countCalf} телят";
+            //Console.WriteLine(text);
+        }
+    }
+}

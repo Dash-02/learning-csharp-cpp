5c42e10d2ab5bc564974fed64a1760d75fe1fdd1
Dash-02 <dasha.semivolos@gmail.com>
2024-07-02 23:33:25 +0300

    hjkl

---
+++ all/all/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace all
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] precipitation = { 0, 1, 0, 0, 2, 1, 3, 0, 1, 0, 1, 0, 0, 0 };
+
+            //int count = 0;
+            //for (int i = 0; i < precipitation.Length; i++)
+            //{
+            //    while (precipitation[i] == 0 && precipitation[i + 1] == 0)
+            //    {
+            //        count++;
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine("Количество первых дней без осадков: " + count);
+
+            //Console.Write("Введите цену за квадратный метр ткани в рублях: ");
+            //double price = double.Parse(Console.ReadLine());
+            //Console.Write("Введите длину ткани в метрах: ");
+            //double length = double.Parse(Console.ReadLine());
+
+            //double wight = 0.8;
+
+            //double area = length * wight;
+
+            //double totalCost = price * area;
+
+            //string result = $"Стоимость за {area} кв. метров составит {totalCost:F2} рублей";
+
+            ////File.WriteAllText("result.txt", result);
+            //Console.WriteLine($"результаты записаны {result}");
+            //Console.ReadLine();
+
+            //int[] rainfall = { 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0 };
+            //int countA = rainfall.TakeWhile(x => x == 0).Count();
+            //int countB = rainfall.Count(x => x == 0);
+
+
+
+            //string result = $"Количество дней без осадков в мае (случай а): {countA}\n" +
+            //    $"Количество дней без осадков в мае (случай а): {countB}";
+
+            //File.WriteAllText("result.txt", result);
+            //Console.WriteLine($"результаты записаны {result}");
+            //Console.ReadLine();
+
+            //int[] rainfall1 = { 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0 };
+            //int countA1 = rainfall1.TakeWhile(x => x == 0).Count();
+            //int countB1 = rainfall1.Count(x => x == 0);
+            //string result = $"Количество дней без осадков в мае (случай а): {countA1}\n" +
+            //    $"Количество дней без осадков в мае (случай а): {countB1}";
+            //Console.WriteLine($"результаты записаны {result}");
+
+            //Console.Write("Введите заработок мальчика за каждую из первых 75 газет: ");
+            //double A = double.Parse(Console.ReadLine());
+            //Console.Write("Введите заработок за каждую последующую газету: ");
+            //double X = double.Parse(Console.ReadLine());
+            //Console.Write("Введите количество проданных газет: ");
+            //double B = double.Parse(Console.ReadLine());
+
+            //double salary = 0.0;
+            //if (B <= 75)
+            //{
+            //    salary = A * B;
+            //}
+            //else if (B > 75)
+            //{
+            //    salary = A * 75 + (B - 75) * X;
+            //}
+
+            //string result = $"Сумма, которую заработает мальчик за {B} проданных газет: {salary:F2} рублей";
+
+            ////File.WriteAllText("result.txt", result);
+            //Console.WriteLine($"результаты записаны {result}");
+            //Console.ReadLine();
+
+            //int[] heights = { 190, 189, 188, 186, 185, 184, 180, 178, 176, 170, 167, 160, 158, 157, 155 };
+            //for (int i = 0; i < heights.Length; i++)
+            //{
+            //    Console.WriteLine($"Рост {i + 1} ученика: {heights[i]}");
+            //}
+
+            //int mesto = 0;
+            //Console.Write("Введите рост нового ученика: ");
+            //int height = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < heights.Length; i++)
+            //{
+            //    if (height > heights.Min() && height < heights.Max())
+            //    {
+            //        if (height < heights[i])
+            //        {
+            //            mesto = i + 1;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Рост нового ученика должен быть больше самого низкого и меньше самого высокого учеников");
+            //        break;
+            //    }
+            //}
+
+            //string result = $"Ученик с ростом {height} см. займет {mesto} место в классе";
+
+            ////File.WriteAllText("result.txt", result);
+            //Console.WriteLine($"результаты записаны {result}");
+            //Console.ReadLine();
+            //double a = 100;
+            //double y = 20;
+            //double total = 0;
+
+            //int year = 1;
+            //int year1 = 0; int year2 = 0; int year3 = 0;
+
+            //while (year1 == 0 || year2 == 0 || year3 == 0)
+            //{
+            //    double curr = a * y;
+            //    total += curr;
+
+            //    if (y > 22 && year1 == 0)
+            //    {
+            //        year1 = year;
+            //    }
+            //    if (a > 120 && year2 == 0)
+            //    {
+            //        year2 = year;
+            //    }
+            //    if (total > 800 && year3 == 0)
+            //    {
+            //        year3 = year;
+            //    }
+
+            //    a = 1.05;
+            //    y = 1.02;
+            //    year++;
+
+            //}
+
+            //string result = $"Год, в котором урожайность превысит 22 цент/га: {year1}\n" +
+            //    $"Год, в котором площадь станет больше 120 гектаров: {year2}\n" +
+            //    $"Год, в котором общий урожай превысит 800 центнеров: {year3}\n";
+
+            //File.WriteAllText("result.txt", result);
+            //Console.WriteLine("Результат записан");
+            //Console.ReadLine();
+
+        }
+    }
+}
---
+++ cpp/cpp/cpp.cpp
@@ -0,0 +1,250 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iomanip>
+using namespace std;
+//int main() {
+
+    /*system("chcp 1251");
+    int count1 = 0, count2 = 0, count3 = 0;
+    string date;
+    string doctor;
+    char choice;
+
+    cout << "Введите дату: ";
+    getline(cin, date);
+
+    do
+    {
+        cout << "Введите доктора (отоларинголог, хирург и окулист): ";
+        getline(cin, doctor);
+
+        if (doctor == "отоларинголог")
+        {
+            count1++;
+        }
+        else if (doctor == "окулист")
+        {
+            count2++;
+        }
+        else if (doctor == "хирург")
+        {
+            count3++;
+        }
+        else {
+            cout << "Неверный ввод, попробуйте еще раз" << endl;
+            continue;
+        }
+
+        cout << "Ввести еще одно посещение? (y - Да, n - Нет): ";
+        cin >> choice;
+        cin.ignore();
+    } while (choice == 'y' || choice == 'Y');
+    ofstream outfile("result.txt");
+    if (outfile.is_open())
+    {
+        outfile << "Итоговые посещения: " << endl;
+        outfile << "Отоларинголог: " << count1 << endl;
+        outfile << "Окулист: " << count2 << endl;
+        outfile << "Хирург: " << count3 << endl;
+
+        outfile.close();
+        cout << "Результат сохранен в result.txt";
+    }
+    return 0;*/
+
+        /*system("chcp 1251");
+        int count = 0;
+        double price;
+        double money;
+        double change = 0.0;
+        double totalCost = 0.0;
+
+        cout << "Введите цену товара: ";
+        cin >> price;
+
+        cout << "Введите количество товара: ";
+        cin >> count;
+
+        totalCost = count * price;
+
+        cout << "Введите сумму для покупки: ";
+        cin >> money;
+        if (money >= totalCost)
+        {
+            change = money - totalCost;
+
+        }
+        else {
+            cout << "Недостаточно денег для покупки";
+            return -1;
+        }
+
+
+
+
+
+        ofstream outfile("result.txt");
+        if (outfile.is_open())
+        {
+            outfile << "Количество товара: " << count << endl;
+            outfile << "Цена за единицу товара: " << price << " рублей" << endl;
+            outfile << "Общая стоимость: " << totalCost << " рублей" << endl;
+            outfile << "Внесенная сумма денег: " << money << " рублей" << endl;
+            outfile << "Сдача: " << change << " рублей" << endl;
+
+            outfile.close();
+            cout << "Результат сохранен в result.txt";
+        }
+
+
+        return 0;*/
+
+    /*struct Student {
+
+        char gender;
+        double height;
+    };*/
+
+    int main() {
+
+        /*system("chcp 1251");
+        int N;
+        cout << "Введите количество студентов: ";
+        cin >> N;
+        vector<Student> students(N);
+        string gender;
+        double height;
+
+        int countBoys = 0;
+        int countGirls = 0;
+
+        double sumBoys = 0.0;
+        double sumGirls = 0.0;
+
+        for (int i = 0; i < N; i++)
+        {
+            Student student;
+
+            cout << "Введите пол студента " << i + 1 << " (м - мужской, ж - женский): ";
+            cin >> student.gender;
+
+            cout << "Введите рост студента " << i + 1 << ": ";
+            cin >> student.height;
+
+            if (student.gender == 'м' || student.gender == 'М')
+            {
+                sumBoys += student.height;
+                countBoys++;
+            }
+            else if (student.gender == 'ж' || student.gender == 'Ж')
+            {
+                sumGirls += student.height;
+                countGirls++;
+            }
+            else {
+                cout << "Некорректный ввод, попробуйте еще раз";
+                i--;
+            }
+        }
+
+        double avgBoys = static_cast<double>(sumBoys) / countBoys;
+        double avgGirls = static_cast<double>(sumGirls) / countGirls;
+
+        ofstream outfile("result.txt");
+        if (outfile.is_open())
+        {
+            outfile << fixed << setprecision(2);
+            outfile << "Средний рост мальчиков в группе: " << avgBoys << " см." << endl;
+            outfile << "Средний рост девочек в группе: " << avgGirls << " см." << endl;
+            outfile.close();
+            cout << "Результат сохранен в result.txt";
+        }
+        return 0;*/
+       /* system("chcp 1251");
+        int N;
+        cout << "Введите количество деталей: ";
+        cin >> N;
+        vector<int> detaiels(N);
+        double diameters;
+        int count = 0;
+        for (int i = 0; i < N; i++)
+        {
+            cout << "Введите диаметр детали " << i + 1 << ": ";
+            cin >> diameters;
+            if (diameters > 4 && diameters < 5)
+            {
+                count++;
+            }
+        }
+        double perc_brak = (static_cast<double>(count) / N) * 100.0;
+        ofstream outfile("result.txt");
+        if (outfile.is_open())
+        {
+            outfile << "Количество деталей: " << N << endl;
+            outfile << "количество браков: " << count << endl;
+            outfile << "Процент брака: " << perc_brak << "%" << endl;
+            outfile.close();
+            cout << "Результат сохранен в result.txt";
+        }
+        return 0;*/
+
+            system("chcp 1251");
+            int N;
+            cout << "Введите количество точек: ";
+            cin >> N;
+            vector<int> points(N);
+            for (int i = 0; i < N; i++)
+            {
+                cout << "Введите координату для точки " << i + 1 << ": ";
+                cin >> points[i];
+            }
+
+            vector<int> segments(4);
+            cout << "Введите координатов отрезков A1, A2, A3, A4: ";
+            for (int& segment : segments)
+            {
+
+                cin >> segment;
+            }
+
+            vector<int> result(3, 0);
+            for (const auto& point : points)
+            {
+                if (point > segments[0] && point <= segments[1])
+                {
+                    result[0]++;
+                }
+                else if (point > segments[1] && point <= segments[2])
+                {
+                    result[1]++;
+                }
+                else if (point > segments[2] && segments[3])
+                {
+                    result[2]++;
+                }
+
+            }
+
+
+
+
+            ofstream outfile("result.txt");
+            if (outfile.is_open())
+            {
+                for (size_t i = 0; i < result.size(); i++)
+                {
+                    outfile << "Количество точек, входящих в отрезок с координатами [" << segments[i] << ": " << segments[i + 1] << "): " << result[i] << endl;
+                }
+                outfile.close();
+                cout << "Результат сохранен в result.txt";
+            }
+
+
+            return 0;
+
+
+        }
+   
+//}

551c7d449fbe8a2ea722329ca3c935ae96bdd092
Dash-02 <dasha.semivolos@gmail.com>
2024-07-02 21:24:37 +0300

    ticket14_sharp_fixed

--- csharp/bilet14_sharp/bilet14_sharp/Program.cs
+++ csharp/bilet14_sharp/bilet14_sharp/Program.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,71 +15,40 @@ namespace bilet14_sharp
     {
         static void Main(string[] args)
         {
-            double cash = 100;
-            int costBull = 10;
-            int costCow = 5;
-            double costCalf = 0.5;
-            int countAll = 100;
-            int count = 0;
-            int countBull = 5;
-            int countCow = 10;
-            int countCalf = 50;
+            int count = 100;
+            int sum = 100;
             double sumCount = 0;
-            double sumCost = 0;
+            int r = 0;
+            int c = 0;
+            int d = 0;
+            double cd = 0;
 
-            int[] maxCounts = {10, 20, 200};
-
-            for (int i = 0; i < maxCounts.Length; i++) 
+            for (int countBull = 0; countBull <= sum / 10; countBull++) 
             {
-                sumCost += maxCounts[i];
-                Console.WriteLine($"sumCost - {sumCost}");
-                for (int j = 0; j < maxCounts.Length; j++)
+                for (int countCow = 0; countCow <= sum / 5; countCow++)
                 {
-                    while (sumCost != 100) {
-                        maxCounts[j]--;
-                        Console.WriteLine(maxCounts[j]);
-                        if (j == 0) {
-                            maxCounts[j] *= costBull;
-                        } else if (j == 1)
+                    for (int countT = 0; countT <= sum / 0.5; countT++) 
+                    {
+                        if (countBull + countCow + countT == count) 
                         {
-                            maxCounts[j] *= costCow;
+                            sumCount = 10 * countBull + 5 * countCow + 0.5 * countT;
+                            if (sumCount == sum)
+                            {
+                                r = countBull;
+                                c = countCow;
+                                d = countT;
+                                cd = sumCount;
+                                Console.WriteLine($"кол-во быков:{countBull} кол-во коров:{countCow} кол-во телят:{countT} сумма:{sumCount}");
+                            }
                         }
-                        else if (j == 2)
-                        {
-                            maxCounts[j] *= (5 / 10);
-                        }
-                        break;
                     }
-
                 }
             }
-            
-            //countBull = Convert.ToInt32(cash / costBull);
-            //countCow = Convert.ToInt32(cash / costCow);
-            //countCalf = Convert.ToInt32(cash / costCalf);
-
-            //Console.WriteLine($"{}");
-            //sumCount = costBull * countBull + costCow * countCow + costCalf * countCalf;
-            //while (sumCount != 100) 
-            //{
-            //    // sumCount = countAll / countBull + countAll / countCow + countAll / countCalf;
-            //    sumCount = costBull * countBull + costCow * countCow + costCalf * countCalf;
-            //    // sumCost = 
-            //    Console.WriteLine($"{sumCount} sumCount");
-            //    if (sumCount == countAll) 
-            //    {
-            //        Console.WriteLine("g");
-            //        break;
-            //    }
-            //    count++;
-            //    countBull--;
-            //    countCow--;
-            //    countCalf--;
-            //    Console.WriteLine($"count - {count}, countBull - {countBull}, countCow - {countCow}, countCalf - {countCalf}");
-            //}
-            //Console.WriteLine("sumCount = " + sumCount);
-            string text = $"На {cash} руб. можно купить {countBull} быков, {countCow} коров, {countCalf} телят";
-            //Console.WriteLine(text);
+            string path = "bilet14_sharp.txt";
+            using (StreamWriter writer = new StreamWriter(path, false)) 
+            {
+                writer.WriteLine($"кол-во быков:{r} кол-во коров:{c} кол-во телят:{d} сумма:{cd}");
+            }
         }
     }
 }
